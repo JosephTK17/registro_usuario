cc4e9bbf3f4fb31fae7bc251dbbdaa5613cacb24
Joseph Steven Barreto Cabrera <jcbarreto@asw.edu.co>
2022-10-12 13:45:38 -0500

    app

--- start/routes.ts
+++ start/routes.ts
@@ -24,7 +24,7 @@ const service = new UsersService();
 
 Route.group(() => {
 
-Route.get('/index', async({request,response}) => {
+Route.get('/', async({request,response}) => {
   response.send('hello')
 })
   
